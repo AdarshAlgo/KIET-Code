c3ec0004d9f2ccdbc67bdaf19273b4ab8022df6b
Adarsh9789 <adarshkatiyar198@gmail.com>
2026-02-18 23:07:42 +0530

    switch

--- A_Eclipse/Advanced_Java_Lab_2025/src/hello/Hello.java
+++ A_Eclipse/Advanced_Java_Lab_2025/src/hello/Hello.java
@@ -1,9 +1,10 @@
+package hello;
 
-public class abc {
+public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("Abc");
+		System.out.println("Hello");
 	}
 
 }
