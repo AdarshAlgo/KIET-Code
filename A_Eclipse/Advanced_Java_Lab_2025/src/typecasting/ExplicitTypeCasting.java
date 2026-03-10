e67ce6410b95d9aceec4c8e79d41637e2822b27e
Adarsh9789 <adarshkatiyar198@gmail.com>
2026-03-10 23:59:08 +0530

    Created Explicit TypeCasting Program

---
+++ A_Eclipse/Advanced_Java_Lab_2025/src/typecasting/ExplicitTypeCasting.java
@@ -0,0 +1,18 @@
+package typecasting;
+
+public class ExplicitTypeCasting {
+
+	public static void main(String[] args) {
+
+		double i = 125;
+		int j = (int) (i);
+		System.out.println(j);
+		
+		char a = 'Z';
+		int b = (int) (a);
+		System.out.println(b);
+		
+		
+	}
+
+}
