f1b1a587e27e4c8a879d439c0444b2881708ccd4
Adarsh9789 <adarshkatiyar198@gmail.com>
2026-03-10 23:54:39 +0530

    Created Implicit Typecasting Program

---
+++ A_Eclipse/Advanced_Java_Lab_2025/src/typecasting/ImplicitTypeCasting.java
@@ -0,0 +1,23 @@
+package typecasting;
+
+public class ImplicitTypeCasting {
+	public static void main(String[] args) {
+		
+		int i = 100;
+		double j;
+		j=i;
+		System.out.println(j);
+		
+		char a ='A';
+		int b =a;
+		System.out.println(b);
+
+		char c ='a';
+		int d =c;
+		System.out.println(d);
+		
+		char e ='0';
+		int f =e;
+		System.out.println(f);
+	}
+}
