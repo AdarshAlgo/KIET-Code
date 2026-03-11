92569127bb850bc4a7b2acf695673461165e278d
Adarsh9789 <adarshkatiyar198@gmail.com>
2026-03-12 01:06:57 +0530

    Created Postfix Prefix Operator to Practice

---
+++ A_Eclipse/Advanced_Java_Lab_2025/src/operators/Postfix_Prefix_Operator.java
@@ -0,0 +1,28 @@
+package operators;
+
+public class Postfix_Prefix_Operator {
+
+	public static void main(String[] args) {
+
+		int a = 4;
+		a++;
+		System.out.println(a);
+		
+		int b = 12;
+		b--;
+		System.out.println(b);
+		
+		int c = 18;
+		++c;
+		System.out.println(c);
+		
+		int d = 22;
+		--d;
+		System.out.println(d);
+		
+		int e = 50;
+		int f = e++ + ++e + ++e + --e + e-- ;
+		System.out.println(f);
+	}
+
+}
