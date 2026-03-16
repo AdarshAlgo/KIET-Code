516dbb0434162bbaa78de518579b8d177d98bbd0
Adarsh9789 <adarshkatiyar198@gmail.com>
2026-03-16 23:41:26 +0530

    Created Logical Operator to Practice

---
+++ A_Eclipse/Advanced_Java_Lab_2025/src/operators/Logical_Operator.java
@@ -0,0 +1,17 @@
+package operators;
+
+public class Logical_Operator {
+
+	public static void main(String[] args) {
+
+		boolean a = true;
+		boolean b = false;
+
+		System.out.println(a && b);
+
+		System.out.println(a || b);
+
+		System.out.println(!a);
+
+	}
+}
