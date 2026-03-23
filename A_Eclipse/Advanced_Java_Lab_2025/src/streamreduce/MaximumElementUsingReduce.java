96ca08094e7c1b91250546104aaf31201f581846
Adarsh9789 <adarshkatiyar198@gmail.com>
2026-03-23 12:05:38 +0530

    Created to find Maximum Element Using Reduce

---
+++ A_Eclipse/Advanced_Java_Lab_2025/src/streamreduce/MaximumElementUsingReduce.java
@@ -0,0 +1,18 @@
+package streamreduce;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MaximumElementUsingReduce {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		List<Integer> numbers = Arrays.asList(5, 12, 3, 21, 8);
+		int max = numbers.stream().reduce(numbers.get(0), (a, b) -> Math.max(a, b));
+
+		System.out.println("The highest number is: " + max);
+
+	}
+
+}
---
+++ A_Eclipse/Advanced_Java_Lab_2025/src/streamreduce/MinimumElementUsingReduce.java
@@ -0,0 +1,10 @@
+package streamreduce;
+
+public class MinimumElementUsingReduce {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
