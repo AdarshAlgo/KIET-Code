f03ba418bb828d6aef68825b6dda65576df8797c
Adarsh9789 <adarshkatiyar198@gmail.com>
2026-03-23 12:09:19 +0530

    Created to find Minimum Element Using Reduce

--- A_Eclipse/Advanced_Java_Lab_2025/src/streamreduce/MinimumElementUsingReduce.java
+++ A_Eclipse/Advanced_Java_Lab_2025/src/streamreduce/MinimumElementUsingReduce.java
@@ -1,9 +1,17 @@
 package streamreduce;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class MinimumElementUsingReduce {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		List<Integer> numbers = Arrays.asList(9, 32, 1, 42, 14);
+		int max = numbers.stream().reduce(numbers.get(0), (a, b) -> Math.min(a, b));
+
+		System.out.println("The smallest number is: " + max);
 
 	}
 
