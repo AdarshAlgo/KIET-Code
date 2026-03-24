464d1d4cc485044da0e15eff1d5a501d8f66a10a
Adarsh9789 <adarshkatiyar198@gmail.com>
2026-03-24 09:52:06 +0530

    Sorted in Ascending Order Using Stream

---
+++ A_Eclipse/Advanced_Java_Lab_2025/src/steamsorting/AscendingOrder.java
@@ -0,0 +1,18 @@
+package steamsorting;
+
+import java.util.*;
+import java.util.stream.*;
+
+public class AscendingOrder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		List<Integer> list = Arrays.asList(5, 2, 9, 1, 3);
+
+		List<Integer> sortedList = list.stream().sorted().collect(Collectors.toList());
+
+		System.out.println(sortedList);
+	}
+
+}
