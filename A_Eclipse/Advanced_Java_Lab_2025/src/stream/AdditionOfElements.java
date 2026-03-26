654b61243697e2ad590657e3dd74fed0d57c89e1
Adarsh9789 <adarshkatiyar198@gmail.com>
2026-03-26 23:59:59 +0530

    Created Addition of Elements

---
+++ A_Eclipse/Advanced_Java_Lab_2025/src/stream/AdditionOfElements.java
@@ -0,0 +1,15 @@
+package stream;
+
+public class AdditionOfElements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+
+		        int sum = list.stream().reduce(0, (a, b) -> a + b);
+
+		        System.out.println("Sum: " + sum);
+		    }
+		}
+
