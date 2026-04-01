b6e1ccef10d7fa419df4116d73322839c14b0a5a
AdarshAlgo <adarshkatiyar198@gmail.com>
2026-04-02 00:09:34 +0530

    Created Average of Array Program

--- A_Eclipse/Advanced_Java_Lab_2025/src/array/AverageOfArray.java
+++ A_Eclipse/Advanced_Java_Lab_2025/src/array/AverageOfArray.java
@@ -2,7 +2,7 @@ package array;
 
 import java.util.Scanner;
 
-public class LargestElementInArray {
+public class AverageOfArray {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -10,19 +10,14 @@ public class LargestElementInArray {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		int[] arr = new int[n];
+		int sum = 0;
 
 		for (int i = 0; i < n; i++) {
 			arr[i] = sc.nextInt();
+			sum += arr[i];
 		}
 
-		int max = arr[0];
-
-		for (int i = 1; i < n; i++) {
-			if (arr[i] > max) {
-				max = arr[i];
-			}
-		}
-
-		System.out.println(max);
+		double avg = (double) sum / n;
+		System.out.println(avg);
 	}
 }
