f3869d16e5b584670846993718f6a499cc794330
AdarshAlgo <adarshkatiyar198@gmail.com>
2026-04-03 23:48:19 +0530

    Smallest Element in Array

---
+++ A_Eclipse/Advanced_Java_Lab_2025/src/array/SmallestElementInArray.java
@@ -0,0 +1,29 @@
+package array;
+
+import java.util.Scanner;
+
+public class SmallestElementInArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+
+		int min = arr[0];
+
+		for (int i = 1; i < n; i++) {
+			if (arr[i] < min) {
+				min = arr[i];
+			}
+		}
+
+		System.out.println(min);
+	}
+
+}
