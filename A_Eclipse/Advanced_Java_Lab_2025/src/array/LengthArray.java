708d3c85b0f942b7e78191a15c6fc94ef6d2c20b
AdarshAlgo <adarshkatiyar198@gmail.com>
2026-04-08 23:34:58 +0530

    Created Length Array Program

---
+++ A_Eclipse/Advanced_Java_Lab_2025/src/array/LengthArray.java
@@ -0,0 +1,13 @@
+package array;
+
+public class LengthArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] arr = { 1, 2, 3, 4, 5 };
+
+		System.out.println("Length: " + arr.length);
+	}
+
+}
