223bb3ff05fea4d604f52d4e0061344c9cf33453
AdarshAlgo <adarshkatiyar198@gmail.com>
2026-04-08 23:36:11 +0530

    Created Multiply Array Program

---
+++ A_Eclipse/Advanced_Java_Lab_2025/src/array/MultiplyArray.java
@@ -0,0 +1,16 @@
+package array;
+
+public class MultiplyArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = { 1, 2, 3, 4 };
+		int product = 1;
+
+		for (int num : arr) {
+			product *= num;
+		}
+
+		System.out.println("Product: " + product);
+	}
+}
