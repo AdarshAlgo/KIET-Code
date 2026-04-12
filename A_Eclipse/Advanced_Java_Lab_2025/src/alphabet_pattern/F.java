7747d06c6ac7c508f7914193133c029c0662a496
AdarshAlgo <adarshkatiyar198@gmail.com>
2026-04-12 23:59:38 +0530

    Created F Alphabet Program

---
+++ A_Eclipse/Advanced_Java_Lab_2025/src/alphabet_pattern/F.java
@@ -0,0 +1,26 @@
+package alphabet_pattern;
+
+import java.util.*;
+public class F {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			Scanner sc = new Scanner(System.in);
+			System.out.print("Enter the Number : ");
+
+			int n = sc.nextInt();
+
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					if (j == 0 || i == (n - 1) / 2 && j<=(n-1)/2|| i==0) {
+						System.out.print("*");
+					}
+					else {
+						System.out.print(" ");
+					}
+				}
+				System.out.println();
+			}
+		}
+
+	}
