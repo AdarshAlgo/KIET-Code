bd386b22af9ed7b8be3a3d779d6b0433cce21f46
AdarshAlgo <adarshkatiyar198@gmail.com>
2026-04-16 12:54:23 +0530

    Created N Alphabet Program

---
+++ A_Eclipse/Advanced_Java_Lab_2025/src/alphabet_pattern/N.java
@@ -0,0 +1,28 @@
+package alphabet_pattern;
+
+import java.util.*;
+
+public class N {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the Number : ");
+
+		int n = sc.nextInt();
+		int i, j;
+
+		for (i = 0; i < n; i++) {
+			for (j = 0; j < n; j++) {
+				if (j == 0 || j == n - 1 || i == j) {
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+
+}
