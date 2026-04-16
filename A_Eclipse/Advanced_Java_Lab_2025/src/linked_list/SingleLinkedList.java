b56b7efcf8835203f04b642884837b6c1904057f
AdarshAlgo <adarshkatiyar198@gmail.com>
2026-04-16 12:42:16 +0530

    Created Z Alphabet Program

---
+++ A_Eclipse/Advanced_Java_Lab_2025/src/linked_list/SingleLinkedList.java
@@ -0,0 +1,19 @@
+package linked_list;
+
+public class Node {
+	int data;
+	Node next;
+	
+	Node(int new_data) {
+		this.data
+	}
+}
+
+public class SingleLinkedList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
