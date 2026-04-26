178a910e9986bdc44e3a399b1684012d1476f0ff
AdarshAlgo <adarshkatiyar198@gmail.com>
2026-04-26 23:42:19 +0530

    Created ADARSH Alphabet Program

---
+++ A_Eclipse/Advanced_Java_Lab_2025/src/alphabet_pattern/ADARSH.java
@@ -0,0 +1,83 @@
+package alphabet_pattern;
+
+import java.util.*;
+public class ADARSH {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.print("Enter the Number : ");
+		Scanner sc = new Scanner(System.in);
+		int n =sc.nextInt();
+		int i,j;
+		
+		for(i=0;i<n;i++) {
+			
+			for( j=0;j<n;j++) {
+				if( i==0 && j>0 && j<n-1 || j==0 && i >0 || j==n-1 && i>0 || i==(n-1)/2) {
+					System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
+				}
+			}
+			System.out.print(" ");
+
+			for( j =0;j<n;j++) {
+				 if(i==0 &&j<n-1 || j==n-1 && i>0 && i<n-1 || i==n-1 && j<n-1 || j==0) {
+					 System.out.print("*");
+				 }
+				 else {
+					 System.out.print(" ");
+				 }
+			 }
+			 System.out.print(" ");
+			 
+			 for( j=0;j<n;j++) {
+					if( i==0 && j>0 && j<n-1 || j==0 && i >0 || j==n-1 && i>0 || i==(n-1)/2) {
+						System.out.print("*");
+					}
+					else {
+						System.out.print(" ");
+					}
+				}
+				System.out.print(" ");
+			 
+			for(j=0;j<n;j++) {
+								
+				if(i==0 && j<(n-1)/2 || j==0 || i==(n-1)/2 && j<(n-1)/2 ||
+					i-j == (n-1)/2&& i>(n-1)/2 || j==(n-1)/2 && i<(n-1)/2 && i>0)
+					System.out.print("*");
+				else if(j>(n-1)/2)
+					continue;
+				else
+					System.out.print(" ");
+			}
+			System.out.print(" ");
+
+			
+			for (j = 0; j < n; j++) {
+				if (i == 0 && j != 0 || j == 0 && i > 0 && i<(n-1)/2 || i==(n-1)/2 && j!=0 && j!=n-1 
+					|| j==n-1 && i>(n-1)/2 && i<n-1 || i == n - 1 && j!=0 && j!=n-1) {
+					System.out.print("*");
+				} else
+					System.out.print(" ");
+			}
+			System.out.print(" ");
+			
+			for ( j = 0; j < n; j++) {
+				if (j == 0 || i == (n - 1) / 2 || j == (n - 1)) {
+					System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
+				}
+			}
+			System.out.print(" ");
+			
+			System.out.println("  ");
+		}
+		sc.close();
+	}
+
+}
