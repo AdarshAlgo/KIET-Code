5a2ae331daa91a18649495b63a322c2a40e6d7d4
AdarshAlgo <adarshkatiyar198@gmail.com>
2026-04-28 18:35:40 +0530

    Created Sum of 1-D Array Program

---
+++ A_Eclipse/Advanced_Java_Lab_2025/src/array1/SumOf1DArray.java
@@ -0,0 +1,16 @@
+package array1;
+
+public class SumOf1DArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a[] = {1,2,3,4,5};
+		int sum=0;
+		for(int i=0;i<5;i++) {
+			sum=sum+a[i];
+		}
+		System.out.println("Sum is : "+ sum);
+	}
+
+}
