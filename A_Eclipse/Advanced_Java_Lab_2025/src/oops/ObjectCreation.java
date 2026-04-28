1ed8b3d547803cbf7d4fb4bb9dc182a0f3f4d6ca
AdarshAlgo <adarshkatiyar198@gmail.com>
2026-04-28 18:39:05 +0530

    Created Object Creation Program

---
+++ A_Eclipse/Advanced_Java_Lab_2025/src/oops/ObjectCreation.java
@@ -0,0 +1,16 @@
+package oops;
+
+class test{
+	int a = 25;
+}
+public class ObjectCreation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		test obj = new test();
+		int n= obj.a;
+		System.out.println("Value from Different Class : "+n);
+	}
+
+}
